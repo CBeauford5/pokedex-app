e22d91776f918d50fc4f6227f71427014a3a9a70
CBeauford5 <103590886+CBeauford5@users.noreply.github.com>
2023-05-15 18:50:10 -0400

    Update scripts.js
    changed formatting of array list and added a comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
+// created a blank array and added Pokemon objects to it
 let pokemonList = [];
-pokemonList = [{ name: 'Bulbasaur', height: '0.7 m', type: ['Grass', 'Poison'] },
+pokemonList = [
+  { name: 'Bulbasaur', height: '0.7 m', type: ['Grass', 'Poison'] },
   { name: 'Charmander', height: '0.6 m', type: 'Fire' },
   { name: 'Charizard', height: '1.7 m', type: ['Fire', 'Flying'] },
   { name: 'Squirtle', height: '0.5 m', type: 'Water' },
   { name: 'Arbok', height: '3.5 m', type: 'Poison'},
-  {name: 'Pikachu', height: '0.4 m', type: 'Electric'},
-  {name: 'Metapod', height: '0.7 m', type: 'Bug'},
-  {name: 'Sandshrew', height: '0.6 m', type: 'Ground'},
-  {name: 'Jigglypuff', height: '0.5 m', type: ['Fairy', 'Normal']},
-  {name: 'Primeape', height: '1 m', type: 'Fighting'}]
+  { name: 'Pikachu', height: '0.4 m', type: 'Electric'},
+  { name: 'Metapod', height: '0.7 m', type: 'Bug'},
+  { name: 'Sandshrew', height: '0.6 m', type: 'Ground'},
+  { name: 'Jigglypuff', height: '0.5 m', type: ['Fairy', 'Normal']},
+  { name: 'Primeape', height: '1 m', type: 'Fighting'}
+];
