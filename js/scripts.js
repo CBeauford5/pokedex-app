ea49efcbb6aeea23b3e20b56241aecc1add641c6
CBeauford5 <103590886+CBeauford5@users.noreply.github.com>
2023-05-15 18:42:32 -0400

    Update scripts.js
    Created an empty array and added various Pokemon objects to the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-alert('Hello world');
-let favoriteFood = 'steak';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList = [{ name: 'Bulbasaur', height: '0.7 m', type: ['Grass', 'Poison'] },
+  { name: 'Charmander', height: '0.6 m', type: 'Fire' },
+  { name: 'Charizard', height: '1.7 m', type: ['Fire', 'Flying'] },
+  { name: 'Squirtle', height: '0.5 m', type: 'Water' },
+  { name: 'Arbok', height: '3.5 m', type: 'Poison'},
+  {name: 'Pikachu', height: '0.4 m', type: 'Electric'},
+  {name: 'Metapod', height: '0.7 m', type: 'Bug'},
+  {name: 'Sandshrew', height: '0.6 m', type: 'Ground'},
+  {name: 'Jigglypuff', height: '0.5 m', type: ['Fairy', 'Normal']},
+  {name: 'Primeape', height: '1 m', type: 'Fighting'}]
