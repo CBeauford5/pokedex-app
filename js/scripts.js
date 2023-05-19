d00f2654a233d006c3958987058bf07436bc2e9d
CBeauford5 <103590886+CBeauford5@users.noreply.github.com>
2023-05-18 21:26:21 -0400

    Update scripts.js
    wrapped pokemonList in a IIFE
    
    created a function to add pokemon with object and key validation
    
    created a function to return the pokemon array
    
    created objects with keys so functions are accessible outside of wrap
    
    converted the for loop to a forEach loop calling the getAll function

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,53 @@
-// created a blank array and added Pokemon objects to it
-let pokemonList = [];
-pokemonList = [
-  { name: 'Bulbasaur', height: '0.7 m', type: ['Grass', 'Poison'] },
-  { name: 'Charmander', height: '0.6 m', type: 'Fire' },
-  { name: 'Charizard', height: '1.7 m', type: ['Fire', 'Flying'] },
-  { name: 'Squirtle', height: '0.5 m', type: 'Water' },
-  { name: 'Arbok', height: '3.5 m', type: 'Poison'},
-  { name: 'Pikachu', height: '0.4 m', type: 'Electric'},
-  { name: 'Metapod', height: '0.7 m', type: 'Bug'},
-  { name: 'Sandshrew', height: '0.6 m', type: 'Ground'},
-  { name: 'Jigglypuff', height: '0.5 m', type: ['Fairy', 'Normal']},
-  { name: 'Primeape', height: '1 m', type: 'Fighting'}
-];
+// IIFE wrap
+let pokemonRepository = (function () {
+  
+  // created a blank array and added Pokemon objects to it
+  let pokemonList = [];
+  pokemonList = [
+    { name: 'Bulbasaur', height: '0.7 m', type: ['Grass', 'Poison'] },
+    { name: 'Charmander', height: '0.6 m', type: 'Fire' },
+    { name: 'Charizard', height: '1.7 m', type: ['Fire', 'Flying'] },
+    { name: 'Squirtle', height: '0.5 m', type: 'Water' },
+    { name: 'Arbok', height: '3.5 m', type: 'Poison' },
+    { name: 'Pikachu', height: '0.4 m', type: 'Electric' },
+    { name: 'Metapod', height: '0.7 m', type: 'Bug' },
+    { name: 'Sandshrew', height: '0.6 m', type: 'Ground' },
+    { name: 'Jigglypuff', height: '0.5 m', type: ['Fairy', 'Normal'] },
+    { name: 'Primeape', height: '1 m', type: 'Fighting' }
+  ];
+
+  //function to add pokemon with object validation
+  function add(pokemon) {
+    if (typeof pokemon === 'object' &&
+      'name' in pokemon &&
+      'height' in pokemon &&
+      'type' in pokemon) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("Not a valid Pokemon object");
+
+    }
+  }
+
+  //function to return pokemon array 
+  function getAll() {
+    return pokemonList;
+  }
+
+  //object created to access functions outside of wrap
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
 // for loop that iterates over the objects in the array and prints them to the DOM
-for (let i=0; i < pokemonList.length; i++){
-  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")")
+pokemonRepository.getAll().forEach(function (pokemon) {
+  document.write(pokemon.name + " (height: " + pokemon.height + ")");
   
   // conditional that searches for pokemon over 2 m in height
-  if (pokemonList[i].height > "2 m") {
+  if (pokemon.height > "2 m") {
     document.write(" - Wow that's big!")
   }
   document.write("<br><br>")
-}
+});
